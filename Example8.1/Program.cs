8d866d50f15c565fe8f6f74d3118910b4ea527b9
Skitt2 <provaid@yandex.ru>
2022-01-20 16:30:37 +0300

    Добавьте файлы проекта.

---
+++ Example8.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Work8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите любую папку на своем компьютере, имеющую вложенные директории. Выведите на консоль ее содержимое и содержимое всех подкаталогов.");
+            string path = @"D:\Temp";
+            Print(path, 0);
+
+            Console.ReadKey();
+        }
+
+        static void Print(string path, int level)
+        {
+            string[] dirs = Directory.GetDirectories(path);
+            string[] files = Directory.GetFiles(path);
+
+            if (dirs.Count() > 0 || files.Count() > 0)
+            {
+                string offset = new string('\t', level);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(offset + s);
+                }
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(offset + s);
+                    Print(s, level + 1);
+                }
+            }
+        }
+    }
+}
